499c6aeee3e5bea0da1110f8df5a8c67c077d3b9
minhbdph24887 <minhbdph24887@gmail.com>
2023-11-29 11:20:30 +0700

    Xoa tat ca cac tep

--- src/main/java/com/example/thuctap/postgresql/repository/CommentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.thuctap.postgresql.repository;
-
-import com.example.thuctap.postgresql.entity.Comment;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CommentRepository extends JpaRepository<Comment, Long> {
-}
